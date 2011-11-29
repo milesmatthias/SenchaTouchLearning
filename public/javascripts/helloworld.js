751440eca3837269d5d80d403426d4c0f939cc90
Miles <miles.matthias@gmail.com>
2011-11-29 11:41:44 -0600

    Added a notes file to keep track of my studying and did some playing around with profiles. Interesting results.

--- public/javascripts/helloworld.js
+++ public/javascripts/helloworld.js
@@ -1,13 +1,28 @@
 new Ext.Application({
 	name: 'HelloWorld',
+    useLoadMask: true,
+    profiles: {
+        phone: function() {
+            return Ext.is.Phone;
+            // This works with the XCode iOS simulator, but not with
+            // changing the user agent in Safari.
+        },
+        tabletPortrait: function() {
+            return Ext.is.Tablet && Ext.orientation == 'portrait';
+            // This works in both the iOS simulator and Safari.
+        },
+        tabletLandscape: function() {
+            return Ext.is.Tablet && Ext.orientation == 'landscape';
+            // This works in both the iOS simulator and Safari.
+        }
+    },
     launch: function() {
         new Ext.Panel({
             fullscreen: true,
             dockedItems: [{xtype:'toolbar', title:'My First App'}],
             layout: 'fit',
             styleHtmlContent: true,
-            html: '<h2>Hello World!</h2>I did it!'
+            html: '<h2>Hello World!</h2>I did it! '+this.getProfile()
         });
-    },
-    useLoadMask: true
+    }
 });
